2db95fd9eafe613d03f3e7fc63f3b8c3f0cc82e8
Daniel Nuriyev <daniel.nuriyev@gmail.com>
2022-01-23 16:35:28 -0500

    scheduler configuration

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 import scheduler
+import sys
+import time
 
 def test():
     print("ok")
 
 if __name__ == "__main__":
     s = scheduler.Scheduler() # TODO: cluster, cloud
-    s.start()
-
     # just testing:
     s.schedule("* * * * *", test)
+
+    while True:
+        time.sleep(10)
--- src/scheduler.py
+++ src/scheduler.py
@@ -1,4 +1,4 @@
-from apscheduler.executors.pool import ProcessPoolExecutor
+from apscheduler.executors.pool import ProcessPoolExecutor, ThreadPoolExecutor
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from apscheduler.schedulers.background import BackgroundScheduler, BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
@@ -12,7 +12,7 @@ from step import Step
 class Scheduler:
     def __init__(self):
 
-        self._scheduler = BackgroundScheduler() # TODO: cluster, cloud
+
 
         # multiprocessing + local slite is just for testing
         cores = cpu_count()
@@ -23,14 +23,20 @@ class Scheduler:
         executors = {
             'default': ProcessPoolExecutor(max_workers=cores)
         }
-        self._scheduler.configure(jobstores=jobstores, executors=executors, timezone=utc)
-
-
-    def start(self): # on a cluster should join an existing cluster
-        self._scheduler.start()
-
-    def pause(self):
-        self._scheduler.pause()
+        job_defaults = {
+            'coalesce': False,
+            'max_instances': 1
+        }
+        conf = {
+            "jobstores": jobstores,
+            "executors": executors,
+            "job_defaults": job_defaults,
+            "timezone": utc
+        }
+        scheduler = BackgroundScheduler(gconfig=conf)  # TODO: cluster, cloud
+        # scheduler.configure(jobstores=jobstores, executors=executors, timezone=utc, job_defaults=job_defaults)
+        scheduler.start()
+        self._scheduler = scheduler
 
     def schedule(self, cron, f): # TODO: cluster, cloud
         self._scheduler.add_job(f, CronTrigger.from_crontab(cron))
